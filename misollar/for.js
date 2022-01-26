badad98f9cabdc89821302dba87c8c5f4f51c4b0
ShaxzodKenjayev <kenjayevshakhzod@gmail.com>
2022-01-26 15:17:00 +0300

    first commit and index.html

---
+++ misollar/for.js
@@ -0,0 +1,6 @@
+for (let i=1; i<=2; i=i+1) {
+    if(i%5 == 0){
+    console.log(i);
+    break;
+    }
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+const numberOfNews = promt("Siz qancha yangilik ko'rdingiz");
+
+const personalNewsDB {
+    count: numberOfNews,
+    news: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = promt("Oxirgi ko'rgan yangiliklardan biri"), b = promt("Unga qancha baho bergan bo'lar edingiz?");
+personalNewsDB.new[a] = b;
+console.log(personalNewsDB);
